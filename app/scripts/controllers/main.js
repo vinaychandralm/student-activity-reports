46c6dedc1128c844fbfe4f77d244cd5fcda6dbf0
sourav <souarv.chatterjee@learningmate.com>
2016-01-21 15:24:22 +0530

    Revert "Added basic JS folder structure"
    This reverts commit 19992cd11afa347a10085ba8362609eed244b9c7.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evidateLearnApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the evidateLearnApp
+ */
+angular.module('evidateLearnApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/student-details.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evidateLearnApp.controller:StudentDetailsCtrl
+ * @description
+ * # StudentDetailsCtrl
+ * Controller of the evidateLearnApp
+ */
+
+angular.module('evidateLearnApp').controller('StudentDetailsCtrl', function () {
+
+    console.log("Inside Student Activity Details Report Controller");
+});
