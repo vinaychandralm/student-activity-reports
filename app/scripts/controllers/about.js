11fd5548366aa9e3e11dcc1ebdb5e80705d437f8
sourav <souarv.chatterjee@learningmate.com>
2016-01-21 15:02:54 +0530

    Added basic folder structure for JS files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name evidateLearnApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the evidateLearnApp
+ */
+angular.module('evidateLearnApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
