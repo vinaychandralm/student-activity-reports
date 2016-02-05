3fecedb4da79f50054b4a61edcf7c733e14f6bf8
shuvajitmukherjee <shuvajit.mukherjee@learningmate.com>
2016-02-05 12:51:43 +0530

    updated

--- src/js/controllers/student-activity-reports.js
+++ src/js/controllers/student-activity-reports.js
@@ -1,22 +1,63 @@
 'use strict'
+
 var sarModule = angular.module('studentActivityReports.studentDetails', []);
 
-sarModule.controller('studentDetailsCtrl', ['$scope','$routeParams', 'getData', function($scope, $routeParams, getData) {
+sarModule.controller('studentDetailsCtrl', ['$scope', '$rootScope','$routeParams', 'getData', 'getEnrollmentStatus', function($scope, $rootScope, $routeParams, getData, getEnrollmentStatus) {
 
-    console.log("Inside studentDetailsCtrl");
+    console.dir("**Inside studentDetailsCtrl**");
 
     $scope.teacherId = $routeParams.teacherId;
-    // console.log($scope.teacherId);
     $scope.details = {};
 
-    $scope.example3settings = {displayProp: 'label', idProp: 'label'};
-    $scope.example1model = [];
+    $rootScope.isblue = false;
 
+    /*
+    * @startDate: holds the start date.
+    * Acceptable date formats: mm-dd-yyyy, mm-dd-yy, ISO formatated string, miliseconds
+    */
+    $scope.startDate = "04-02-2016";
+
+    /*
+    * @endDate: holds the start date.
+    * Acceptable date formats: mm-dd-yyyy, mm-dd-yy, ISO formatated string, miliseconds
+    */
+    $scope.endDate = "04-02-2016";
+
+    /*
+    * @courseArr: Courses received from server
+    * TODO:: modify object structure as per data received.
+    */
+    $scope.courseArr = [
+        {
+            id: 0,
+            name: "Grade 2 Language Arts"
+        },
+        {
+            id: 1,
+            name: "Grade 5 Mathematics"
+        },
+        {
+            id: 2,
+            name: "Grade 10 Integrated Math"
+        },
+        {
+            id: 3,
+            name: "SINET: Biology A (Flex)"
+        }
+    ];
+
+    /*
+    * @enrollmentArr: Enrollment array
+    */
+    $scope.enrollmentArr = getEnrollmentStatus.get();
+
+    // Success callback
     var handleSuccess = function(data, status) {
         $scope.details = data;
         console.log(status, $scope.details.courses._get);
     };
 
+    // Error callback
     var handleError = function(err, status) {
         $scope.details = {};
         console.log(status, err);
@@ -24,11 +65,7 @@ sarModule.controller('studentDetailsCtrl', ['$scope','$routeParams', 'getData', 
 
     getData._get($scope.teacherId).success(handleSuccess).error(handleError);
 
-    $(".date-picker").datepicker();
-
-    $(".date-picker").on("change", function () {
-        var id = $(this).attr("id");
-        var val = $("label[for='" + id + "']").text();
-        $("#msg").text(val + " changed");
-    });
+    $scope.$watch('selectedDate', function() {
+        console.log($scope.selectedDate);
+    }, true);
 }]);
